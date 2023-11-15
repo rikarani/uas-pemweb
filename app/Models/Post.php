318a669eedf963f79765401a9620ff279be41642
rikarani <46773591+rikarani@users.noreply.github.com>
2023-11-15 16:56:54 +0700

    Add Category, Post, and User models with relationships and guarded attributes.

---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ["id"];
+    protected $with = ["author", "category"];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, "user_id");
+    }
+
+    public function getRouteKeyName()
+    {
+        return "slug";
+    }
+}
