748f6776c73ad097aab355048a11f49185f89358
rikarani <46773591+rikarani@users.noreply.github.com>
2023-11-15 23:48:14 +0700

    Add search functionality to posts page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,25 @@
 @extends('layout')
 
 @section('content')
-    <h1 class="mb-3">{{ $title }}</h1>
+    <h1 class="mb-3 text-center">{{ $title }}</h1>
+
+    <div class="row justify-content-center mb-3">
+        <div class="col-md-6">
+            <form action="/posts">
+                @if (request('category'))
+                    <input type="hidden" name="category" value="{{ request('category') }}">
+                @endif
+                @if (request('author'))
+                    <input type="hidden" name="author" value="{{ request('author') }}">
+                @endif
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Search" name="search"
+                        value="{{ request('search') }}">
+                    <button class="btn btn-danger" type="submit">Search</button>
+                </div>
+            </form>
+        </div>
+    </div>
 
     {{-- Latest Post --}}
     @if ($posts->count())
@@ -13,9 +31,9 @@
                         class="text-decoration-none text-dark">{{ $posts[0]->title }}</a>
                 </h3>
                 <small class="text-body-secondary">
-                    <p>By <a href="/posts/author/{{ $posts[0]->author->username }}"
+                    <p>By <a href="/posts?author={{ $posts[0]->author->username }}"
                             class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a
-                            href="/posts/category/{{ $posts[0]->category->slug }}"
+                            href="/posts?category={{ $posts[0]->category->slug }}"
                             class="text-decoration-none">{{ $posts[0]->category->name }}</a>
                         {{ $posts[0]->created_at->diffForHumans() }}</p>
                 </small>
@@ -23,38 +41,41 @@
                 <a href="/post/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read More</a>
             </div>
         </div>
+        {{-- Latest Post --}}
+
+        {{-- Remaining Post --}}
+        <div>
+            <div class="row">
+                @foreach ($posts->skip(1) as $post)
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <div class="position-absolute px-3 py-2 text-white"
+                                style="background-color: rgba(0, 0, 0, 0.5)">
+                                <a href="/posts?category={{ $post->category->slug }}"
+                                    class="text-decoration-none text-white">{{ $post->category->name }}</a>
+                            </div>
+                            <img src="https://source.unsplash.com/500x400?{{ $post->category->slug }}" class="card-img-top"
+                                alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $post->title }}</h5>
+                                <small class="text-body-secondary">
+                                    <p>By <a href="/posts?author={{ $post->author->username }}"
+                                            class="text-decoration-none">{{ $post->author->name }}</a>
+                                        {{ $post->created_at->diffForHumans() }}</p>
+                                </small>
+                                <p class="card-text">{{ $post->excerpt }}
+                                </p>
+                                <a href="/post/{{ $post->slug }}" class="btn btn-primary">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     @else
         <p class="text-center fs-4">No Post Found</p>
     @endif
-    {{-- Latest Post --}}
+    {{-- Remaining Post --}}
 
-    {{-- Remaining Post --}}
-    <div>
-        <div class="row">
-            @foreach ($posts->skip(1) as $post)
-                <div class="col-md-4 mb-3">
-                    <div class="card">
-                        <div class="position-absolute px-3 py-2 text-white" style="background-color: rgba(0, 0, 0, 0.5)">
-                            <a href="/posts/category/{{ $post->category->slug }}"
-                                class="text-decoration-none text-white">{{ $post->category->name }}</a>
-                        </div>
-                        <img src="https://source.unsplash.com/500x400?{{ $post->category->slug }}" class="card-img-top"
-                            alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $post->title }}</h5>
-                            <small class="text-body-secondary">
-                                <p>By <a href="/posts/author/{{ $post->author->username }}"
-                                        class="text-decoration-none">{{ $post->author->name }}</a>
-                                    {{ $post->created_at->diffForHumans() }}</p>
-                            </small>
-                            <p class="card-text">{{ $post->excerpt }}
-                            </p>
-                            <a href="/post/{{ $post->slug }}" class="btn btn-primary">Read More</a>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    </div>
-    {{-- Remaining Post --}}
+    {{ $posts->links() }}
 @endsection
