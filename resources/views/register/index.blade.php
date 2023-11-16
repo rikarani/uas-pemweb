70bd62090ad649076eea86750686b97ca31462ff
rikarani <46773591+rikarani@users.noreply.github.com>
2023-11-16 13:59:50 +0700

    Add form validation and submission to registration page

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -6,24 +6,47 @@
             <main class="form-registration w-100 m-auto">
                 <h1 class="h3 mb-3 fw-normal text-center">Daftar dulu woy</h1>
 
-                <form>
+                <form action="/register" method="POST">
+                    @csrf
                     <div class="form-floating">
-                        <input type="text" class="form-control rounded-top" name="name" id="name" placeholder="Name">
+                        <input type="text" class="form-control rounded-top @error('name') is-invalid @enderror"
+                            name="name" id="name" placeholder="Name" value="{{ old('name') }}">
                         <label for="name">Name</label>
+                        @error('name')
+                            <div class="invalid-feedback mb-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="text" class="form-control" name="username" id="username" placeholder="Username">
+                        <input type="text" class="form-control @error('username') is-invalid @enderror" name="username"
+                            id="username" placeholder="Username" value="{{ old('username') }}">
                         <label for="username">Username</label>
+                        @error('username')
+                            <div class="invalid-feedback mb-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="email" class="form-control" name="email" id="email"
-                            placeholder="name@example.com">
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                            id="email" placeholder="name@example.com" value="{{ old('email') }}">
                         <label for="email">Email address</label>
+                        @error('email')
+                            <div class="invalid-feedback mb-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="password" class="form-control rounded-bottom" name="password" id="password"
-                            placeholder="Password">
+                        <input type="password" class="form-control rounded-bottom @error('password') is-invalid @enderror"
+                            name="password" id="password" placeholder="Password">
                         <label for="password">Password</label>
+                        @error('password')
+                            <div class="invalid-feedback mb-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
 
                     <button class="btn btn-primary w-100 py-2" type="submit">Daftar</button>
