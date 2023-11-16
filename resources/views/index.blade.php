20f60015a85f2fd6b653863a8d458875f2b7741d
rikarani <46773591+rikarani@users.noreply.github.com>
2023-11-16 23:41:16 +0700

    Refactor index.blade.php to use layout and section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,25 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layout.layout')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Blog Sederhana</title>
-    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-
-    {{-- Bootstrap Icon --}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    {{-- Bootstrap Icon --}}
-</head>
-
-<body>
-    @include('components.navbar')
-
-    <div class="container mt-3">
-        <h1 class="fs-4">Selamat Datang di Blog Sederhana ini, ini adalah projek UAS Mata Kuliah Pemograman Web</h1>
-    </div>
-
-    {{-- @include('components.footer') --}}
-</body>
-
-</html>
+@section('content')
+    <h1 class="mt-5 fs-4">Selamat Datang di Blog Sederhana ini, ini adalah projek UAS Mata Kuliah Pemograman Web</h1>
+@endsection
