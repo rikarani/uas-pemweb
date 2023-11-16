ac76cf134842e75999a9cffb5adc3f1dae72e4b0
rikarani <46773591+rikarani@users.noreply.github.com>
2023-11-16 10:30:30 +0700

    Add login page with form

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.layout')
+
+
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-md-5">
+            <main class="form-signin w-100 m-auto">
+                <h1 class="h3 mb-3 fw-normal text-center">Login dulu woy</h1>
+
+                <form>
+                    <div class="form-floating">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                        <label for="floatingInput">Email address</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                        <label for="floatingPassword">Password</label>
+                    </div>
+
+                    <button class="btn btn-primary w-100 py-2" type="submit">Login</button>
+                </form>
+
+                <small class="d-block text-center mt-3">Blom Punya Akun? <a href="/register">gas daftar!!!!</a></small>
+            </main>
+        </div>
+    </div>
+@endsection
