9ea400b584ed4c7fe1d1eca77223bea1763c90f6
rikarani <46773591+rikarani@users.noreply.github.com>
2023-11-16 10:31:55 +0700

    Add Bootstrap Icon and comment out footer include

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Blog Sederhana</title>
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+
+    {{-- Bootstrap Icon --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    {{-- Bootstrap Icon --}}
 </head>
 
 <body>
@@ -14,6 +18,8 @@
     <div class="container mt-3">
         <h1 class="fs-4">Selamat Datang di Blog Sederhana ini, ini adalah projek UAS Mata Kuliah Pemograman Web</h1>
     </div>
+
+    {{-- @include('components.footer') --}}
 </body>
 
 </html>
