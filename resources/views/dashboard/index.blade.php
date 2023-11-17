6aac03cc24f3244221a4dea1a042630b1b4bc457
rikarani <46773591+rikarani@users.noreply.github.com>
2023-11-17 23:53:36 +0700

    Refactored dashboard index.blade.php file.

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,7 +1,5 @@
 @extends('dashboard.layout.layout')
 
-@section('content')
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Welcome Back, {{ auth()->user()->name }}</h1>
-    </div>
+@section('header')
+    <h1 class="h2">Welcome Back, {{ auth()->user()->name }}</h1>
 @endsection
