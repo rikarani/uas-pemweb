b6bc4cf4675d8ca1ad07f2bdbb50278aa9544284
rikarani <46773591+rikarani@users.noreply.github.com>
2023-11-17 18:09:19 +0700

    Update dashboard index view with new welcome message

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,5 +1,7 @@
-@extends('layout.layout')
+@extends('dashboard.layout.layout')
 
 @section('content')
-    <h1>Halo, {{ auth()->user()->name }}</h1>
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Welcome Back, {{ auth()->user()->name }}</h1>
+    </div>
 @endsection
