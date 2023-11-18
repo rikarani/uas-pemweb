29c1266677078d4d1ab4ee51030a0c1e443ba612
rikarani <46773591+rikarani@users.noreply.github.com>
2023-11-18 21:34:13 +0700

    Refactor layout.blade.php file

--- resources/views/dashboard/layout/layout.blade.php
+++ resources/views/dashboard/layout/layout.blade.php
@@ -35,10 +35,7 @@
             @include('dashboard.partials.sidebar')
 
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                <div
-                    class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    @yield('header')
-                </div>
+                @yield('header')
 
                 @yield('content')
             </main>
