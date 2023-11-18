ea1bd4f49a967d91f574bde396b97c13c7babe30
rikarani <46773591+rikarani@users.noreply.github.com>
2023-11-18 21:26:12 +0700

    Add title and content to index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,12 @@
 @extends('layout.layout')
 
+@section('title')
+    <h1>Selamat Datang di Blog Sederhana ini, ini adalah projek UAS Mata Kuliah Pemograman Web</h1>
+@endsection
+
 @section('content')
-    <h1 class="mt-5 fs-4">Selamat Datang di Blog Sederhana ini, ini adalah projek UAS Mata Kuliah Pemograman Web</h1>
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab dolor voluptatum neque nihil voluptatibus incidunt!
+            Dolor iusto quis deleniti, molestiae quisquam natus rerum ipsam odio at blanditiis itaque eum aliquid.</p>
+    </div>
 @endsection
