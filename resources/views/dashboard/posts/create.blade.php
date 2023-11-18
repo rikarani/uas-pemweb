f2967bbcd8aa64422d8206f67b7866cd3586678f
rikarani <46773591+rikarani@users.noreply.github.com>
2023-11-19 02:34:37 +0700

    Update header in create.blade.php

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -1,7 +1,9 @@
 @extends('dashboard.layout.layout')
 
 @section('header')
-    <h1 class="h2">Tambah Post Baru</h1>
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Tambah Post Baru</h1>
+    </div>
 @endsection
 
 @section('content')
