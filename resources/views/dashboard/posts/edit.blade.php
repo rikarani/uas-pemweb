0d91e8168b6aba9a51d7457c6219efd77bd071ef
rikarani <46773591+rikarani@users.noreply.github.com>
2023-11-19 22:06:37 +0700

    Fix form enctype in edit.blade.php

--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -8,13 +8,13 @@
 
 @section('content')
     <div class="col-lg-8">
-        <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="mb-5">
+        <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="mb-5" enctype="multipart/form-data">
             @method('put')
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Judul</label>
-                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
-                    value="{{ old('title', $post->title) }}" required autofocus />
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title"
+                    name="title" value="{{ old('title', $post->title) }}" required autofocus />
                 @error('title')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -44,6 +44,23 @@
                 </select>
             </div>
             <div class="mb-3">
+                <label for="image" class="form-label">Gambar <span style="font-size: 11px">(Opsional)</span></label>
+                <input type="hidden" name="oldImage" value="{{ $post->image }}">
+                @if ($post->image)
+                    <img src="{{ asset('storage/' . $post->image) }}" id="preview"
+                        class="img-fluid d-block mb-3 col-lg-8">
+                @else
+                    <img id="preview" class="img-fluid d-block mb-3 col-lg-8">
+                @endif
+                <input class="form-control @error('image') is-invalid @enderror" type="file" id="image"
+                    name="image" onchange="previewImage()">
+                @error('image')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+            <div class="mb-3">
                 <label for="body" class="form-label">Body</label>
                 @error('body')
                     <p class="text-danger">{{ $message }}</p>
@@ -68,5 +85,15 @@
         document.addEventListener("trix-file-accept", (e) => {
             e.preventDefault();
         })
+
+        function previewImage() {
+            const inputImage = document.getElementById("image");
+            const previewImage = document.getElementById("preview")
+
+            const [file] = inputImage.files;
+            if (file) {
+                previewImage.src = URL.createObjectURL(file);
+            }
+        }
     </script>
 @endsection
