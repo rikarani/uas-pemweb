94606854d2297564a474e5a73a8d40661c582a33
rikarani <46773591+rikarani@users.noreply.github.com>
2023-11-19 13:13:42 +0700

    Add user seeder with default values

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->create([
+            "name" => "Phasya Ananta",
+            "username" => "erikaaaa",
+            "email" => "phasya@gmail.com",
+            "password" => Hash::make("rahasia")
+        ]);
+
         $this->call(CategorySeeder::class);
         // $this->call([PostSeeder::class, CategorySeeder::class, UserSeeder::class]);
     }
