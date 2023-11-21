8f2227f35490f0b6314fec3572302042bce52c3d
rikarani <46773591+rikarani@users.noreply.github.com>
2023-11-22 02:57:52 +0700

    ganti logic

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -15,7 +15,7 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (auth()->guest() || auth()->user()->username !== "erikaaaa") {
+        if (auth()->guest() || !auth()->user()->is_admin) {
             abort(403);
         }
 
