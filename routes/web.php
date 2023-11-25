bb4198e6815c5fc2b8a1bed6f218aa1db190f828
rikarani <46773591+rikarani@users.noreply.github.com>
2023-11-26 03:31:08 +0700

    Refactor routes and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
-use App\Http\Controllers\AdminCategoryController;
 use App\Http\Controllers\AdminUserController;
 use App\Http\Controllers\DashboardMateriController;
-use App\Models\Category;
-use App\Http\Controllers\DashboardPostController;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\RepoController;
 use Illuminate\Support\Facades\Route;
@@ -30,20 +26,6 @@ Route::get("/about", function () {
     return view("about", ["page" => "About"]);
 });
 
-Route::get("/posts", [PostController::class, "index"]);
-Route::get("/post/{post}", [PostController::class, "show"]);
-
-Route::get("/categories", function () {
-    return view("categories", [
-        "page" => "Kategori",
-        "title" => "Kategori Postingan",
-        "categories" => Category::all()
-    ]);
-});
-
-Route::get("/register", [RegisterController::class, "index"])->middleware("guest");
-Route::post("/register", [RegisterController::class, "store"]);
-
 Route::get("/login", [LoginController::class, "index"])->middleware("guest")->name("login");
 Route::post("/login", [LoginController::class, "authenticate"]);
 Route::post("/logout", [LoginController::class, "logout"]);
@@ -52,15 +34,9 @@ Route::middleware("auth")->get("/dashboard", function () {
     return view("dashboard.index", ["page" => "Dashboard"]);
 });
 
-Route::get("/dashboard/posts/generate", [DashboardPostController::class, "generate"]);
-Route::resource("/dashboard/posts", DashboardPostController::class)->middleware("auth");
-
-Route::get("/dashboard/categories/generate", [AdminCategoryController::class, "generate"]);
-Route::resource("/dashboard/categories", AdminCategoryController::class)->except("show")->middleware("admin");
-
 Route::resource("/dashboard/users", AdminUserController::class)->except("show")->middleware("admin");
 
-Route::get("/repo", [RepoController::class, "index"]);
-Route::get("/repo/{course}", [RepoController::class, "show"]);
+Route::get("/course", [RepoController::class, "index"]);
+Route::get("/course/{course}", [RepoController::class, "show"]);
 
 Route::resource("/dashboard/materi", DashboardMateriController::class);
