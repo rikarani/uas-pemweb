9dd09731dc8d4238f6780f7231bca8acc14164d7
rikarani <46773591+rikarani@users.noreply.github.com>
2023-11-26 03:21:42 +0700

    Remove registration link from login page

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -46,8 +46,6 @@
 
                     <button class="btn btn-primary w-100 py-2" type="submit">Login</button>
                 </form>
-
-                <small class="d-block text-center mt-3">Blom Punya Akun? <a href="/register">gas daftar!!!!</a></small>
             </main>
         </div>
     </div>
