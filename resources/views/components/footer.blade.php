87e164c52040c95a362fc859cb390a99c3e2969f
rikarani <46773591+rikarani@users.noreply.github.com>
2023-11-28 02:25:59 +0700

    Update footer text

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,7 +2,7 @@
 
 <footer class="footer mt-auto py-3 bg-body-tertiary">
     <div class="container d-flex align-items-center justify-content-between">
-        <span class="text-body-secondary">&copy; {{ $carbon::now()->year }} - Kerjaan Siapa?</span>
+        <span class="text-body-secondary">&copy; {{ $carbon::now()->year }} - Kelompok 5</span>
         <a href="https://github.com/rikarani/uas-pemweb" target="_blank">
             <i class="bi bi-github" style="font-size: 28px; color:black"></i>
         </a>
